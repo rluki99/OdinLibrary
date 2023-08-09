54cd386c470c649df69040cda3ec83e285346c28
rluki99 <120097849+rluki99@users.noreply.github.com>
2023-08-09 21:08:32 +0200

    feat: modal with js code

--- src/js/main.js
+++ src/js/main.js
@@ -11,8 +11,41 @@ function Book(title, author, pages, read) {
 }
 
 const addBookToLibrary = () => {
-	
+	myLibrary.push(theHobbit)
+	myLibrary.push(theTester)
 }
 
 const theHobbit = new Book('The Hobbit', 'Tolkien', '295', 'read')
+const theTester = new Book('The Tester', 'Test', '2137', 'read')
 console.log(theHobbit.info())
+
+addBookToLibrary()
+console.log(myLibrary)
+
+
+//MODAL
+
+const addBookBtn = document.querySelector('#add-book')
+const closeModalBtn = document.querySelector('.books__modal-close')
+const modal = document.querySelector('.books__modal')
+const modalOverlay = document.querySelector('.books__modal-overlay')
+
+const openModal = () => {
+	modal.classList.add('books__modal--active')
+	modalOverlay.classList.add('books__modal-overlay--active')
+}
+
+const closeModal = () => {
+	modal.classList.remove('books__modal--active')
+	modalOverlay.classList.remove('books__modal-overlay--active')
+}
+
+const clickOutsideModal = (e) => {
+	if (e.target == modal) {
+		closeModal()
+	}
+}
+
+addBookBtn.addEventListener('click', openModal)
+closeModalBtn.addEventListener('click', closeModal)
+window.addEventListener('click', clickOutsideModal)
